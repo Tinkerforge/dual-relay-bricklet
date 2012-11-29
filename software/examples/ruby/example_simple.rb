4be327b69b290987f68f53f8dc8fcf1b11ee8cc6
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 11:39:44 +0100

    Make Ruby examples Protocol 2.0 compatible

--- software/examples/ruby/example_simple.rb
+++ software/examples/ruby/example_simple.rb
@@ -10,10 +10,11 @@ HOST = 'localhost'
 PORT = 4223
 UID = '6hL' # Change to your UID
 
-ipcon = IPConnection.new HOST, PORT # Create IP connection to brickd
-dr = BrickletDualRelay.new UID # Create device object
-ipcon.add_device dr # Add device to IP connection
-# Don't use device before it is added to a connection
+ipcon = IPConnection.new # Create IP connection
+dr = BrickletDualRelay.new UID, ipcon # Create device object
+
+ipcon.connect HOST, PORT # Connect to brickd
+# Don't use device before ipcon is connected
 
 # Turn relays alternating on/off for 10 times with 1 second delay
 for i in 0..9
@@ -28,4 +29,3 @@ end
 
 puts 'Press key to exit'
 $stdin.gets
-ipcon.destroy
