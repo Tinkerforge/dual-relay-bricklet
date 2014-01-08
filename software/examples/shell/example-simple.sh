b17aaa9ee14d4f644714b35c9cb778799a71378d
Matthias Bolte <matthias@tinkerforge.com>
2014-01-08 13:16:06 +0100

    shell: Make example toggle the relays

--- software/examples/shell/example-simple.sh
+++ software/examples/shell/example-simple.sh
@@ -4,5 +4,10 @@
 # change to your UID
 uid=dbs
 
-# turn relay 1 on and relay 2 off
-tinkerforge call dual-relay-bricklet $uid set-state true false
+# Turn relays alternating on/off for 10 times with 1 second delay
+for i in 0 1 2 3 4; do
+	sleep 1
+	tinkerforge call dual-relay-bricklet $uid set-state true false
+	sleep 1
+	tinkerforge call dual-relay-bricklet $uid set-state false true
+done
