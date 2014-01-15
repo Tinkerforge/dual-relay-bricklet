7520657ba9c6a1b71abf4e8e9597766a83ae4b9f
Matthias Bolte <matthias@tinkerforge.com>
2014-01-15 11:14:59 +0100

    php: Use const for constant values

--- software/examples/php/ExampleSimple.php
+++ software/examples/php/ExampleSimple.php
@@ -6,14 +6,14 @@ require_once('Tinkerforge/BrickletDualRelay.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickletDualRelay;
 
-$host = 'localhost';
-$port = 4223;
-$uid = '6hL'; // Change to your UID
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = '6hL'; // Change to your UID
 
 $ipcon = new IPConnection(); // Create IP connection
-$dr = new BrickletDualRelay($uid, $ipcon); // Create device object
+$dr = new BrickletDualRelay(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
 // Turn relays alternating on/off for 10 times with 1 second delay
