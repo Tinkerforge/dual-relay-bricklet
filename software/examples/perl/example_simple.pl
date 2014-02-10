9f6fb5fffca23a45b406acb0f9e61c57a53e9d4c
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-10 17:17:34 +0100

    perl: fixed namespace

--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -7,8 +7,8 @@ use constant HOST => 'localhost';
 use constant PORT => 4223;
 use constant UID => 'joQ'; # Change to your UID
 
-my $ipcon = IPConnection->new(); # Create IP connection
-my $dr = BrickletDualRelay->new(&UID, $ipcon); # Create device object
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $dr = Tinkerforge::BrickletDualRelay->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
