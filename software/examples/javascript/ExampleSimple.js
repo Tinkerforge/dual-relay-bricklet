c8658f360207f8356fb46dd5cb9af223d3b36358
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-18 15:59:34 +0100

    javascript: examples fixed

--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -1,12 +1,11 @@
-var IPConnection = require('Tinkerforge/IPConnection');
-var BrickletDualRelay = require('Tinkerforge/BrickletDualRelay');
+var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
 var UID = 'fBk';// Change to your UID
 
-var ipcon = new IPConnection();// Create IP connection
-var dr = new BrickletDualRelay(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var dr = new Tinkerforge.BrickletDualRelay(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
@@ -15,7 +14,7 @@ ipcon.connect(HOST, PORT,
 );// Connect to brickd
 
 // Don't use device before ipcon is connected
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Turn relays on/off
         dr.setState(true, false);
