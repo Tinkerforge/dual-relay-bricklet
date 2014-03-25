40910ebb4a850c25d2ef6ec9ee33fde66597715d
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-03-25 12:38:45 +0100

    matlab: add examples

---
+++ software/examples/matlab/matlab_example_simple.m
@@ -0,0 +1,27 @@
+function matlab_example_simple
+    import com.tinkerforge.IPConnection;
+    import com.tinkerforge.BrickletDualRelay;
+
+    HOST = 'localhost';
+    PORT = 4223;
+    UID = '9y5'; % Change to your UID
+    
+    ipcon = IPConnection(); % Create IP connection
+    dr = BrickletDualRelay(UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Turn relays alternating on/off for 10 times with 1 second delay
+    for i = 1:10
+        pause(1);
+        if mod(i, 2)
+            dr.setState(true, false);
+        else
+            dr.setState(false, true);
+        end
+    end
+
+    input('\nPress any key to exit...\n', 's');
+    ipcon.disconnect();
+end
---
+++ software/examples/matlab/octave_example_simple.m
@@ -0,0 +1,26 @@
+function octave_example_simple
+    more off;
+    
+    HOST = "localhost";
+    PORT = 4223;
+    UID = "9y5"; % Change to your UID
+
+    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
+    dr = java_new("com.tinkerforge.BrickletDualRelay", UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don"t use device before ipcon is connected
+
+    % Turn relays alternating on/off for 10 times with 1 second delay
+    for i = 1:10
+        pause(1);
+        if mod(i, 2)
+            dr.setState(true, false);
+        else
+            dr.setState(false, true);
+        end
+    end
+
+    input("\nPress any key to exit...\n", "s");
+    ipcon.disconnect();
+end
