101e7cf9962ab81059f251b375445d18c0676aa9
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:08:49 +0100

    Add go examples.

---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"tinkerforge/dual_relay_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Dual Relay Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	dr, _ := dual_relay_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Turn relays alternating on/off 10 times with 1 second delay
+	for i := 0; i < 5; i++ {
+		time.Sleep(1000 * time.Millisecond)
+		dr.SetState(true, false)
+		time.Sleep(1000 * time.Millisecond)
+		dr.SetState(false, true)
+	}
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
