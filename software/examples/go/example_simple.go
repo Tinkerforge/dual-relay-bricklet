2f0bf3c677f8e30668c3d45abe355bf45028e8ca
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:17:02 +0100

    go: Change import path again.

--- software/examples/go/example_simple.go
+++ software/examples/go/example_simple.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/tinkerforge/go-api-bindings/dual_relay_bricklet"
-	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/Tinkerforge/go-api-bindings/dual_relay_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
 	"time"
 )
 
