e21d12aa3b356b29f0b59b58f01f0eb3e5b1e09c
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:33:32 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,5 +1,9 @@
-use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{dual_relay_bricklet::*, ip_connection::IpConnection};
+use std::{io, error::Error};
+use std::thread;
+use std::time::Duration;
+use tinkerforge::{ip_connection::IpConnection, 
+                  dual_relay_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,15 +14,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let dr = DualRelayBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Turn relays alternating on/off 10 times with 1 second delay
-    for i in 0..5 {
-        thread::sleep(Duration::from_millis(1000));
-        dr.set_state(true, false);
-        thread::sleep(Duration::from_millis(1000));
-        dr.set_state(false, true);
-    }
+		// Turn relays alternating on/off 10 times with 1 second delay
+		for i in 0..5{
+			thread::sleep(Duration::from_millis(1000));
+			dr.set_state(true, false);
+			thread::sleep(Duration::from_millis(1000));
+			dr.set_state(false, true);
+		}
 
     println!("Press enter to exit.");
     let mut _input = String::new();
